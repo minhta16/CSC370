d6ca23153269142b4d04a0b2dc5fc83b79545aec
Minh <minhta16@augustana.edu>
2018-09-25 13:16:05 -0500

    Almost finish guess method

--- Program04/src/HangmanGame.java
+++ Program04/src/HangmanGame.java
@@ -17,21 +17,24 @@ public class HangmanGame {
 	private ArrayList<Character> displayPattern;
 	private TreeSet<String> currentWords;
 	private TreeSet<Character> letterGuessed;
-	private TreeMap<ArrayList<Character>, TreeSet<String>> curPattern;
+	private TreeMap<ArrayList<Character>, TreeSet<String>> currentPatternMap;
 
 	public HangmanGame (List<String> dictionary, int length, int maximum) {
 		if (length <= 1 || maximum <= 0) {
 			throw new IllegalArgumentException("Illegal length / maximum guesses.");
 		}
 		currentWords = new TreeSet<String>();
+		currentPatternMap = new TreeMap<ArrayList<Character>, TreeSet<String>>();
 		for (String word : dictionary) {
 			if (word.length() == length) {
 				currentWords.add(word);
 			}
 		}
+		displayPattern = new ArrayList<Character>();
 		for (int i = 0; i < length; i++) {
 			displayPattern.add('-');
 		}
+		
 		guessLeft = maximum;
 		letterGuessed = new TreeSet<Character>();
 	}
@@ -69,11 +72,56 @@ public class HangmanGame {
 			throw new IllegalArgumentException("Character already guessed.");
 		}
 		letterGuessed.add(guess);
-		for (String word : currentWords) {
-			
+		makeGuessTree(guess);
+		
+		ArrayList<Character> longestSet = findKeyOfLongestWordSet();
+		currentWords = currentPatternMap.get(longestSet);		
+		displayPattern = longestSet;
+		if (displayPattern.contains(guess)) {
+			System.out.println("Sorry, there are no " + guess + "'s.");
+		} else {
+			System.out.print("Yes, there ");
+			TreeSet<Character> set = new TreeSet<Character>(displayPattern);
+			if (displayPattern.size() != set.size()) {
+				System.out.println("are " + (displayPattern.size() - set.size()) + " " + guess + "'s");
+			} else {
+				System.out.println("is one " + guess);
+			}
 		}
+		System.out.println();
 		return 0;
 	}
 	
+	private void makeGuessTree(char guess) {
+		for (String word : currentWords) {
+			ArrayList<Character> wordPattern = new ArrayList<Character>();
+			for (int i = 0; i < word.length(); i++) {
+				if (word.charAt(i) == guess) {
+					wordPattern.add(guess);
+				} else {
+					wordPattern.add('-');
+				}
+			}
+			if (currentPatternMap.containsKey(wordPattern)) {
+				TreeSet<String> newWordSet = currentPatternMap.get(wordPattern);
+				newWordSet.add(word);
+				currentPatternMap.put(wordPattern, newWordSet);
+			} else {
+				TreeSet<String> newWordSet = new TreeSet<String>();
+				newWordSet.add(word);
+				currentPatternMap.put(wordPattern, newWordSet);
+			}
+		}
+	}
+	
+	private ArrayList<Character> findKeyOfLongestWordSet() {
+		ArrayList<Character> longestSet = new ArrayList<Character>();
+		for (ArrayList<Character> key : currentPatternMap.keySet()) {
+			if (currentPatternMap.get(key).size() > longestSet.size()) {
+				longestSet = key;
+			}
+		}
+		return longestSet;
+	}
 	
 }
